59a1def8859d17c18a5ef16f30b76c0494da1991
Peter Stefek <peterlikesemail@gmail.com>
2026-09-01 20:24:03 -0700

    screwable body

--- Assets/Scripts/ScrewableBody.cs
+++ Assets/Scripts/ScrewableBody.cs
@@ -4,7 +4,16 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class ScrewableBody : MonoBehaviour
 {
+    public static int NextScrewableBodyId = 0;
+
     public HashSet<Screw> AttachedScrews = new HashSet<Screw>();
+    public int Id = -1;
+
+    private void Awake()
+    {
+        Id = NextScrewableBodyId;
+        NextScrewableBodyId++;
+    }
 
     public Rigidbody GetRigidbody()
     {
